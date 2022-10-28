c0b7d1dfb1d781b47e86c238b437580f491646c2
Moscolape <chunkums088@gmail.com>
2022-10-28 13:31:45 +0100

    Add the newly created folders and files for our project, alongside firebase utils and libraries that have been installed

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,66 @@
+import {initializeApp} from 'firebase/app';
+import {
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
+
+// My app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBn1A4rIbRcfSiOQPHXJemHwvsorclIFNk",
+  authDomain: "mosco-clothing-store-db.firebaseapp.com",
+  projectId: "mosco-clothing-store-db",
+  storageBucket: "mosco-clothing-store-db.appspot.com",
+  messagingSenderId: "39151990309",
+  appId: "1:39151990309:web:5f97b6886721c79e4c71cc"
+};
+
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    } catch(error) {
+      console.log('error encountered creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+}
