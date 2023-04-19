0fd6c2e5015e8b32456c24c417aa88824b64d1d7
Chukwunenye Moses <chunkums088@gmail.com>
2023-04-19 13:22:21 +0100

    Update checkout-item.component.tsx

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -29,9 +29,9 @@ const CheckoutItem: FC<CheckoutItemProps> = memo(({cartItem}) => {
       </ImageContainer>
       <BaseSpan> {name} </BaseSpan>
       <Quantity>
-        <Arrow onClick={decreaseItems}>&minus;</Arrow>
+        <Arrow onClick={decreaseItems}>-</Arrow>
         <Value>{quantity}</Value>
-        <Arrow onClick={increaseItems}>&#43;</Arrow>
+        <Arrow onClick={increaseItems}>+</Arrow>
       </Quantity>
       <BaseSpan> {price}</BaseSpan>
       <RemoveButton onClick={clearItems}>&#10005;</RemoveButton>
